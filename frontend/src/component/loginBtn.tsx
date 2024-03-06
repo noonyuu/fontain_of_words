064e50de59117bddfcec0af898cf0ab44b63f488
saya514 <tokunosima8008@gmail.com>
2024-03-06 11:20:41 +0900

    [add]タイトル、ログイン文字 [update]間隔調整

--- frontend/src/component/loginBtn.tsx
+++ frontend/src/component/loginBtn.tsx
@@ -10,17 +10,20 @@ const loginBtn:React.FC<Btnprops> = ({name,icon}) => {
   return (
     <div>
     <button className='
+        md:
         rounded-[50px]
         border-solid
-        border-2 
+        border-[1px]
         border-black 
         bg-white
         h-[31.24px]
         w-[245px]
-        mt-[20px]'
+        mt-[20px]
+        xl:
+        '
     >
-        <div className='inline-block float-left	ml-[5%] pt-[2.5px]'>{icon}</div>
-        <div className='inline-block'>{name}</div>
+        <div className='inline-block float-left	ml-[5%] pt-[1%]'>{icon}</div>
+        <div className='inline-block text-sm'>{name}</div>
     </button>
     </div>
   )
--- frontend/src/page/RoginPage.tsx
+++ frontend/src/page/RoginPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import LoginBtn from '../component/loginBtn'
-import TitleName from '../component/TitleName'
 // アイコンのインポート
 import { FcGoogle } from "react-icons/fc";          // Google
 import { BsDiscord } from "react-icons/bs";         // Discord
@@ -12,8 +11,9 @@ import { IoLogoMicrosoft } from "react-icons/io5";  // Microsoft
 const RoginPage = () => {
   return (
     <main>
-      <TitleName />
-      <div>
+      <div className='Kaisei Tokumin text-main text-5xl text-center my-[44px]'>言葉の泉</div>
+      <div className='Kaisei Tokumin text-[32px] text-center mb-[40px]'>ログイン</div>
+      <div className='text-center'>
       <LoginBtn name="Google" icon={<FcGoogle />} />
       <LoginBtn name="Discord" icon={<BsDiscord color="#5865F2"/>} />
       <LoginBtn name="GitHub" icon={<SiGithub />} />
