3d5af6bed1a601b59bfcb0c7163b1403cd29bd79
shimizu <noon3m.op.sh@gmail.com>
2024-03-09 00:15:05 +0900

    [update]mergeミス修正

--- frontend/src/page/HomePage.tsx
+++ frontend/src/page/HomePage.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   
   return (
     <main
-      className={`h-full ${profileModal ?  "bg-gray-200 bg-opacity-50" : "bg-white"} bg-bottom	bg-cover bg-mainBg lg:bg-mainBg_Lg`}
+      className={`h-full ${profileModal ?  "bg-gray-200 bg-opacity-50" : "bg-white"} bg-bottom	bg-cover bg-mainBg lg:bg-mainBg_Lg`}>
         <div className='bg-white w-dvw h-[51.97px] lg:h-[70px]'>header</div>
         <div className='Kaisei Tokumin text-center text-white text-5xl lg:text-8xl mt-40 lg:mt-20'>言葉の泉</div>
         <div className='absolute bottom-0 h-[25px] w-dvw'><Footer /></div>
