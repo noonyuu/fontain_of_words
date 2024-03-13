7895700badccf1a4cfeff6edcbc06b4b97cca094
shimizu <noon3m.op.sh@gmail.com>
2024-03-14 01:37:09 +0900

    [add]仮のログイン時の遷移先を設定

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
 // import { Login } from "./pages/Login";
 import Secret from "./routes/Secret";
-import Notfound from "./page/NotFound"
+import Notfound from "./page/NotFound";
 import LoginPage from "./page/LoginPage";
 import NewAccountPage from "./page/NewAccountPage";
 import { useEffect } from "react";
-import { Refresh_Token } from "./auth/Auth";
+import { GetInfo, Refresh_Token } from "./auth/Auth";
 
 const Router = () => {
   const navigate = useNavigate();
@@ -13,17 +13,24 @@ const Router = () => {
   useEffect(() => {
     // 現在のパスが/ならloginへ遷移
     if (window.location.pathname === "/") {
-      navigate("/LoginPage");
+      console.log("login");
+      const token = GetInfo();
+      // console.log(token);
+      if (token) {
+        console.log("token");
+        console.log(token);
+        navigate("/secret/HomePage");
+      } else {
+        navigate("/LoginPage");
+      }
     }
-    // if (GetInfo()) {
-    //   navigate("/LoginPage");
-    // }
+
     Refresh_Token();
   }, [navigate]);
   return (
     <Routes>
-      {/* <Route path="/" element={<LoginPage />} /> */}
       <Route path="/NewAccountPage" element={<NewAccountPage />} />
+      {/* <Route path="/" element={<LoginPage />} /> */}
       <Route path="/LoginPage" element={<LoginPage />} />
       <Route path="secret/*" element={<Secret />} />
       <Route path="*" element={<Notfound />} />
