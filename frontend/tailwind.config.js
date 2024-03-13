97c0d3fd95dd6de5e0ce69f4c16270da5b5fc26f
shimizu <noon3m.op.sh@gmail.com>
2024-03-14 01:36:14 +0900

    [update]htmlタグに高さを設定

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,12 +12,12 @@ module.exports = {
       },
       backgroundImage: {
         mainBg: "url('./img/mainBack_mobile.png')",
-        mainBg_md: "url('./img/mainBg_md.png')",
-        mainBg_lg: "url('./img/mainBg-lg.png')",
-        voiceBg: "url('./img/voiceBack.png')",
-        voiceBg_md: "url('./img/voiceBack_md.png')",
-        voiceBg_lg: "url('./img/voiceBg_lg.png')",
-        circle: "url('./img/Ellipse.png')",
+        // mainBg_md: "url('./img/mainBg_md.png')",
+        mainBg_Lg: "url('./img/mainBg-lg.png')",
+        // voiceBg: "url('./img/voiceBack.png')",
+        // voiceBg_md: "url('./img/voiceBack_md.png')",
+        // voiceBg_lg: "url('./img/voiceBg_lg.png')",
+        // circle: "url('./img/Ellipse.png')"
       },
       dropShadow: {
         mainShadow: "0px 3px 1px rgba(0,4,4,0.25)",
