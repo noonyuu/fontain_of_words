e51b5810a820a75dbf9ce8bcf44aac9460b76fdd
shimizu <noon3m.op.sh@gmail.com>
2024-03-14 01:40:51 +0900

    [add]背景を追加した

--- frontend/src/page/NotFound.tsx
+++ frontend/src/page/NotFound.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Notfound = () => {
   return (
-    <div>
+    <div className="h-full overflow-y-auto	bg-mainBg bg-cover bg-fixed bg-bottom pb-[30px] pt-16 lg:bg-mainBg_Lg">
       NotFound
     </div>
-  )
-}
+  );
+};
 
-export default Notfound
+export default Notfound;
