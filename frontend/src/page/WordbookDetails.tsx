cba8ae28e340287ef11a556725df498097799ff5
shimizu <noon3m.op.sh@gmail.com>
2024-03-16 02:56:21 +0000

    [add]戻るボタンの動き追加

--- frontend/src/page/WordbookDetails.tsx
+++ frontend/src/page/WordbookDetails.tsx
@@ -8,9 +8,10 @@ import { LiaTimesSolid } from "react-icons/lia";
 import { FaLink } from "react-icons/fa6";
 
 import { Get_words, } from "../scripts/word";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const WordbookDetails = () => {
+  const history = useNavigate();
     const [title, setTitle] = useState("");
     const [wordid, setID] = useState("");
     const [link, setLink] = useState("");
@@ -51,7 +52,7 @@ const WordbookDetails = () => {
         SetText("AIに聞いています");
 
         //{ message: 'スイッチは、回路内の電流の経路を変更する電気機械式デバイスです。電流をオンまたはオフしたり、回路の別の部分に経路を切り替えるために使用できます。', result: 'ok', status: 'success' }
-        fetch("https://localhost:8443/app/ai?refresh=0", {
+        fetch("https://fountain-of-words.noonyuu.com/app/ai?refresh=0", {
             method: "POST",
             body: JSON.stringify({
                 "Text": word_str
@@ -74,7 +75,7 @@ const WordbookDetails = () => {
         RefreshSetText("AIに聞いています");
 
         //{ message: 'スイッチは、回路内の電流の経路を変更する電気機械式デバイスです。電流をオンまたはオフしたり、回路の別の部分に経路を切り替えるために使用できます。', result: 'ok', status: 'success' }
-        fetch("https://localhost:8443/app/ai?refresh=1", {
+        fetch("https://fountain-of-words.noonyuu.com/app/ai?refresh=1", {
             method: "POST",
             body: JSON.stringify({
                 "Text": word_str
@@ -91,7 +92,7 @@ const WordbookDetails = () => {
     const Detail = (index: number) => {
         console.log(words[index]["Word"]);
         setTitle(words[index]["Word"]);
-        fetch("https://localhost:8443/app/description/" + words[index]["WordID"], {
+        fetch("https://fountain-of-words.noonyuu.com/app/description/" + words[index]["WordID"], {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -141,110 +142,122 @@ const WordbookDetails = () => {
     };
 
     return (
-        <main className="h-full overflow-y-auto	bg-fixed bg-bottom pb-[30px] pt-16">
-            <div
-                className={`${modal ? "h-full bg-gray-50 opacity-50 lg:bg-white lg:opacity-100" : ""}`}
-            >
-                <div className="ml-4 h-5 pt-4">
-                    {/* TODO:仮アイコン */}
-                    <BsArrowReturnLeft size={24} />
+      <main className="h-full overflow-y-auto	bg-fixed bg-bottom pb-[30px] pt-16">
+        <div
+          className={`${modal ? "h-full bg-gray-50 opacity-50 lg:bg-white lg:opacity-100" : ""}`}
+        >
+          <div className="ml-4 h-5 pt-4">
+            {/* TODO:仮アイコン */}
+            <BsArrowReturnLeft size={24} onClick={() => history(-1)} />
+          </div>
+          <h2 className="relative mr-6 mt-8 flex h-10 w-24 items-center justify-center bg-main font-bold leading-4 text-white after:absolute after:-right-10 after:top-0 after:block after:h-10 after:border-[20px] after:border-r-[20px] after:border-solid after:border-main after:border-r-transparent">
+            #IT
+          </h2>
+          <div className="lg:flex lg:h-[calc(100%-7.75rem)]">
+            {/* 検索欄 */}
+            <div className="w-full lg:w-2/5">
+              <div className="my-2 flex justify-center lg:ml-8 lg:justify-start">
+                <div className="relative mt-2 rounded-md shadow-sm">
+                  {/* icon */}
+                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center rounded-md bg-gray-200 pl-3">
+                    <span className="flex items-center text-gray-500 sm:text-sm">
+                      <IoMdSearch />
+                    </span>
+                  </div>
+                  {/* 入力 */}
+                  <input
+                    type="text"
+                    name="price"
+                    id="price"
+                    className="block w-full rounded-md border-0 bg-gray-200 py-1.5 pl-9 pr-20 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 lg:pr-32"
+                    placeholder="検索"
+                  />
                 </div>
-                <h2 className="relative mr-6 mt-8 flex h-10 w-24 items-center justify-center bg-main font-bold leading-4 text-white after:absolute after:-right-10 after:top-0 after:block after:h-10 after:border-[20px] after:border-r-[20px] after:border-solid after:border-main after:border-r-transparent">
-                    #IT
-                </h2>
-                <div className="lg:flex lg:h-[calc(100%-7.75rem)]">
-                    {/* 検索欄 */}
-                    <div className="w-full lg:w-2/5">
-                        <div className="my-2 flex justify-center lg:ml-8 lg:justify-start">
-                            <div className="relative mt-2 rounded-md shadow-sm">
-                                {/* icon */}
-                                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center rounded-md bg-gray-200 pl-3">
-                                    <span className="flex items-center text-gray-500 sm:text-sm">
-                                        <IoMdSearch />
-                                    </span>
-                                </div>
-                                {/* 入力 */}
-                                <input
-                                    type="text"
-                                    name="price"
-                                    id="price"
-                                    className="block w-full rounded-md border-0 bg-gray-200 py-1.5 pl-9 pr-20 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 lg:pr-32"
-                                    placeholder="検索"
-                                />
-                            </div>
-                        </div>
-                        {/* list */}
-                        <div className="mt-4 lg:ml-8 lg:mr-24">
-                            <ul className="w-full">
-                                <li className="w-full border-t-[1px] border-gray-300"></li>
-                                {words.map((word, index) => (
-                                    <li
-                                        key={index}
-                                        className="flex w-full items-center justify-between border-b-[1px] border-gray-300 py-4 pl-8 lg:border-[1px]"
-                                        onClick={() => Detail(index)}
-                                    >
-                                        <span>{word["Word"]}</span>
-                                        <button type="button" className="pr-4">
-                                            <CiCircleQuestion size={24} />
-                                        </button>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                    {/* ディスクトップ単語説明 */}
-                    <div
-                        className={`${selected ? "mr-4 hidden h-full w-3/5 rounded-[3rem] border-2 shadow-lg lg:inline-block" : "hidden"}`}
+              </div>
+              {/* list */}
+              <div className="mt-4 lg:ml-8 lg:mr-24">
+                <ul className="w-full">
+                  <li className="w-full border-t-[1px] border-gray-300"></li>
+                  {words.map((word, index) => (
+                    <li
+                      key={index}
+                      className="flex w-full items-center justify-between border-b-[1px] border-gray-300 py-4 pl-8 lg:border-[1px]"
+                      onClick={() => Detail(index)}
                     >
-                        <div className="flex h-[10%] items-center justify-end rounded-t-[3rem] border-b-2 bg-main pr-8">
-                            <LiaTimesSolid size={32} onClick={handleClick} />
-                        </div>
-                        <div className="h-[70%] rounded-b-[3rem]">
-                            <div className="mt-3 text-center text-[3rem] underline">
-                                {title}
-                            </div>
-                            <div className="mx-10 mt-8 h-[95%] overflow-auto text-center text-2xl">
-                                {parse(r)}
-                                <button className={`${isshow ? "" : "hidden"}`} onClick={() => {
-                                    CallAI(wordid);
-                                }}>{btn_text}</button>
-                                <button className={`${refresh_isshow ? "" : "hidden"}`} onClick={() => {
-                                    Call_Refresh_AI(wordid);
-                                }}>{refresh_btn_text}</button>
-                            </div>
-                            <div className="mx-10 mt-4 flex text-2xl text-blue-300">
-                                <FaLink color="gray" size={32} />
-                                <a href={link} target="_blank" rel="noopener noreferrer">検索リンク</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                      <span>{word["Word"]}</span>
+                      <button type="button" className="pr-4">
+                        <CiCircleQuestion size={24} />
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-
-            {/* モバイル単語説明 */}
+            {/* ディスクトップ単語説明 */}
             <div
-                className={
-                    selected
-                        ? "absolute left-1/2 top-1/2 z-50 mr-4 h-3/4 w-4/5 -translate-x-1/2 -translate-y-1/2 rounded-[2rem] border-2 bg-white shadow-lg lg:hidden"
-                        : "hidden"
-                }
+              className={`${selected ? "mr-4 hidden h-full w-3/5 rounded-[3rem] border-2 shadow-lg lg:inline-block" : "hidden"}`}
             >
-                <div className="flex h-[10%] items-center justify-end rounded-t-[2rem] border-b-2 bg-main pr-8">
-                    <LiaTimesSolid size={32} onClick={handleClick} />
+              <div className="flex h-[10%] items-center justify-end rounded-t-[3rem] border-b-2 bg-main pr-8">
+                <LiaTimesSolid size={32} onClick={handleClick} />
+              </div>
+              <div className="h-[70%] rounded-b-[3rem]">
+                <div className="mt-3 text-center text-[3rem] underline">
+                  {title}
                 </div>
-                <div className="h-[60%] rounded-b-[2rem]">
-                    <div className="mt-3 text-center text-2xl underline">{title}</div>
-                    <div className="text-1xl mx-6 mt-8 h-[95%] overflow-auto text-center">
-                        {parse(r)}
-                        {parse(r)}
-                    </div>
-                    <div className="text-1xl mx-10 mt-4 flex text-blue-300">
-                        <FaLink color="gray" size={24} />
-                        {link}
-                    </div>
+                <div className="mx-10 mt-8 h-[95%] overflow-auto text-center text-2xl">
+                  {parse(r)}
+                  <button
+                    className={`${isshow ? "" : "hidden"}`}
+                    onClick={() => {
+                      CallAI(wordid);
+                    }}
+                  >
+                    {btn_text}
+                  </button>
+                  <button
+                    className={`${refresh_isshow ? "" : "hidden"}`}
+                    onClick={() => {
+                      Call_Refresh_AI(wordid);
+                    }}
+                  >
+                    {refresh_btn_text}
+                  </button>
                 </div>
+                <div className="mx-10 mt-4 flex text-2xl text-blue-300">
+                  <FaLink color="gray" size={32} />
+                  <a href={link} target="_blank" rel="noopener noreferrer">
+                    検索リンク
+                  </a>
+                </div>
+              </div>
             </div>
-        </main>
+          </div>
+        </div>
+
+        {/* モバイル単語説明 */}
+        <div
+          className={
+            selected
+              ? "absolute left-1/2 top-1/2 z-50 mr-4 h-3/4 w-4/5 -translate-x-1/2 -translate-y-1/2 rounded-[2rem] border-2 bg-white shadow-lg lg:hidden"
+              : "hidden"
+          }
+        >
+          <div className="flex h-[10%] items-center justify-end rounded-t-[2rem] border-b-2 bg-main pr-8">
+            <LiaTimesSolid size={32} onClick={handleClick} />
+          </div>
+          <div className="h-[60%] rounded-b-[2rem]">
+            <div className="mt-3 text-center text-2xl underline">{title}</div>
+            <div className="text-1xl mx-6 mt-8 h-[95%] overflow-auto text-center">
+              {parse(r)}
+              {parse(r)}
+            </div>
+            <div className="text-1xl mx-10 mt-4 flex text-blue-300">
+              <FaLink color="gray" size={24} />
+              {link}
+            </div>
+          </div>
+        </div>
+      </main>
     );
 };
 
